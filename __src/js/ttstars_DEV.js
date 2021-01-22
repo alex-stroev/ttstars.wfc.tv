ce4d3e6dc8da7a941f3172e04ccfd9388d96d4a9
Alex Stroev <stroev.alexander@gmail.com>
2021-01-22 22:16:24 +0300

    Передача get-параметров в скрытые инпуты + проверка загружаемых фоток на фронте

--- __src/js/ttstars_DEV.js
+++ __src/js/ttstars_DEV.js
@@ -67,6 +67,100 @@ $(document).ready(function () {
         $this.siblings(".jsHint").hide()
     });
 
+    ////////////////////////////////////////////////////////////////
+    //  Передаем параметры из URL в скрытые поля формы
+    ////////////////////////////////////////////////////////////////
+
+    var queryStr = window.location.search,
+        queryArr = queryStr.replace('?', '').split('&'),
+        queryParams = [];
+
+    for (var i = 0, qArrLength = queryArr.length; i < qArrLength; i++) {
+        var qArr = queryArr[i].split('='),
+            qName = [qArr[0]],
+            qValue = [qArr[1]];
+
+
+        $('<input>').attr({
+            type: 'hidden',
+            name: 'others[' + qName + ']',
+            value: qValue
+        }).appendTo($("#wfc_form"));
+
+        // console.log(qName + " - " +qValue)
+    }
+
+
+    ////////////////////////////////////////////////////////////////
+    //  Загрузка файлов
+    ////////////////////////////////////////////////////////////////
+    var iFile = $("input[type=file]"),
+        jsFilelimit = $(".jsFilelimit"),
+        iFilePlaceholder = iFile.siblings("span"),
+        BODY = $("body"),
+        maxFileQuan = 5,
+        maxTotalFileSizes = 11010048, //10.5 мБ, про запас
+        defaultFilesLimitText = "Не более 5 фотографий, общим объемом не более 10 мб";
+
+    var specOverlay = $('<div>').css({
+        "position": "fixed",
+        "top": "0",
+        "left": "0",
+        "width": "100%",
+        "height": "100%",
+        "background": "rgba(0, 0, 0, 0.8)",
+        "color": "#fff",
+        "font-size": "24px",
+        "display": "-webkit-box",
+        "display": "-ms-flexbox",
+        "display": "flex",
+        "-webkit-box-align": "center",
+        "-ms-flex-align": "center",
+        "align-items": "center",
+        "text-align": "center",
+        "-webkit-box-pack": "center",
+        "-ms-flex-pack": "center",
+        "justify-content": "center",
+        "padding": "20%",
+        "box-sizing": "border-box",
+        "cursor": "pointer",
+        "z-index": "9999"
+    });
+
+
+    iFile.change(function (event) {
+        var filesQuan = this.files.length;
+        var totalFileSizes = 0;
+        console.log("Кол-во файлов:" + filesQuan);
+        for (i = 0; i < filesQuan; i++) {
+            totalFileSizes = totalFileSizes + this.files[i].size;
+            console.log("Вес файла " + i + ": " + totalFileSizes);
+        }
+        // console.log("Вес файлов:" + totalFileSizes);
+
+        if (!filesQuan) {
+            jsFilelimit.html(defaultFilesLimitText);
+        } else if (filesQuan > maxFileQuan) {
+            iFile.val();
+            iFilePlaceholder.text("Фото*");
+            jsFilelimit.html(defaultFilesLimitText);
+            specOverlay.appendTo(BODY).html("Не более 5 файлов! Вы загрузили " + filesQuan + ".<br> Попробуйте еще раз))").click(function (event) {
+                $(this).remove();
+            });
+        } else if (totalFileSizes > maxTotalFileSizes) {
+            // jsFilelimit.html("Выбрано " + filesQuan + " фото, выберите не более" + maxFileQuan);
+            iFile.val();
+            iFilePlaceholder.text("Фото*");
+            jsFilelimit.html(defaultFilesLimitText);
+            specOverlay.appendTo(BODY).html("Суммарный объем фото&nbsp;&mdash; не&nbsp;более 10&nbsp;мб! Вы загрузили " + parseFloat(totalFileSizes / 1024 / 1024).toFixed(2) + " мБ. <br> Попробуйте еще раз))").click(function (event) {
+                $(this).remove();
+            });
+        } else {
+            jsFilelimit.html("Выбрано " + filesQuan + " фото общим объемом " + parseFloat(totalFileSizes / 1024 / 1024).toFixed(2) + " мб");
+        }
+    });
+
+    /////////////////////////////////////////////////////
     // end ready
 });
 
